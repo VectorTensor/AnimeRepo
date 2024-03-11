14be182ac01ddfbdc32971f84e3469eca3a0e27f
VectorTensor <prayashthapa15@gmail.com>
2024-03-11 20:44:16 +0545

    feat(post): post dto added

---
+++ AnimeRepo/AnimeRepo/JsonDto/AnimePostDto.cs
@@ -0,0 +1,17 @@
+﻿namespace AnimeRepo.JsonDto;
+
+public class AnimePostDto
+{
+    
+    
+    public string AnimeName { get; set; }
+    public List<EGenre> Genre { get; set; }
+
+    public AnimePostDto(string a, List<EGenre> g)
+    {
+        AnimeName = a;
+        Genre = g;
+    }
+    
+    
+}
